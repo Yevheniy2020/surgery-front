f1a06f655170b7232d7cab3c4ba627b1687aa76b
Yevhenii Mynenko <68693183+Yevheniy2020@users.noreply.github.com>
2024-12-03 10:52:58 +0200

    fixed app paths

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MainPage from "components/screen/Main";
-import ViewMedicalCase from "components/screen/view/ViewMedicalCase";
-import ViewDiagnoses from "components/screen/view/ViewDiagnoses";
-import ViewDoctors from "components/screen/view/ViewDoctors";
-import ViewInsurance from "components/screen/view/ViewInsurance";
-import ViewOperations from "components/screen/view/ViewOperations";
-import ViewResearch from "components/screen/view/ViewResearch";
+import MainPage from "./components/screen/Main";
+import ViewMedicalCase from "./components/screen/view/ViewMedicalCase";
+import ViewDiagnoses from "./components/screen/view/ViewDiagnoses";
+import ViewDoctors from "./components/screen/view/ViewDoctors";
+import ViewInsurance from "./components/screen/view/ViewInsurance";
+import ViewOperations from "./components/screen/view/ViewOperations";
+import ViewResearch from "./components/screen/view/ViewResearch";
 
 function App() {
   return (
